d2aee7723bd4acb5465f3593016d0d65b7564a9f
Tim <timothy_may@hotmail.co.uk>
2021-07-31 16:56:29 +0100

    Added rules for en passant

--- chessplot/__main__.py
+++ chessplot/__main__.py
@@ -5,8 +5,8 @@ from chessplot.parse import pgn_to_file
 def main():
     pgn_to_file(
         file_path="C:/Users/Tim/Documents/test_match_v02.pgn",
-        output_file_path="C:/Users/Tim/Documents/test_match_v02_output.pdf",
-        output_file_format="pdf"
+        output_file_path="C:/Users/Tim/Documents/test_match_v02_output.gif",
+        output_file_format="gif"
     )
 
 
