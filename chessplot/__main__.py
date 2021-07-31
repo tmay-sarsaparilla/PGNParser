a15d939e426a8b89a649c1f3e72ffff1704d0c1b
Tim <timothy_may@hotmail.co.uk>
2021-07-31 17:17:13 +0100

    Altered main.py to allow for passing of arguments

--- chessplot/__main__.py
+++ chessplot/__main__.py
@@ -1,14 +1,28 @@
 
-from chessplot.parse import pgn_to_file
+import argparse
+import sys
+
+from .parse import pgn_to_file
 
 
-def main():
+def main(args):
+    parser = argparse.ArgumentParser(description="Chessplot", usage="Reads and plots PGN files to .gif or .pdf")
+    parser.add_argument("-file_path", type=str, required=True, help="Location of PGN file to be parsed")
+    parser.add_argument("-output_file_path", type=str, required=True, help="Location of the plot file to be created")
+    parser.add_argument(
+        "-output_file_format",
+        type=str,
+        required=True,
+        help="Format of output file. Valid formats: 'gif', 'pdf'"
+    )
+    args = parser.parse_args(args)
+
     pgn_to_file(
-        file_path="C:/Users/Tim/Documents/test_match_v02.pgn",
-        output_file_path="C:/Users/Tim/Documents/test_match_v02_output.gif",
-        output_file_format="gif"
+        file_path=args.file_path,
+        output_file_path=args.output_file_path,
+        output_file_format=args.output_file_format
     )
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
